3fe96c1df795f5cbb592d9b35f6188394a7dbf2e
toytoy08 <toy31327@gmail.com>
2024-10-08 01:08:56 +0700

    Implement Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="">
+      <div className="bg-slate-900 py-3">
+        <p className="text-center text-slate-400">Copyright 2024 © My IDEA</p>
+      </div>
+    </div>
   )
 }
 
